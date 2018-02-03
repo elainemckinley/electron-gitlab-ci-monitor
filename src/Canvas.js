00c726e9e82b365fb1bb938823bcb73a42b8e1a3
Austin McKinley <austinmckinley0@gmail.com>
2018-02-03 10:20:16 -0700

    Display cards for each project off of dummy data

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
 import './Canvas.css'
+import ProjectCard from './ProjectCard/ProjectCard.js'
 
 class Canvas extends Component {
+  constructor() {
+    super()
+    this.state = {
+      projects: [{
+        location: 'austinmckinley/Testing-CI',
+        displayName: 'Testing CI'
+      }],
+      apiBase: 'https://gitlab.com/'
+    }
+  }
+
   componentDidMount() {
-    // Check config file
+    // TODO set state of projects from input file
   }
 
   render() {
     return (
       <div className="Canvas">
-        <h1>Hello!</h1>
+        {
+          this.state.projects.map((project, index) =>
+            <ProjectCard
+              key={index}
+              project={project}
+            />)
+        }
       </div>
     )
   }
--- src/ProjectCard/ProjectCard.js
+++ src/ProjectCard/ProjectCard.js
@@ -1,16 +1,31 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './ProjectCard.css'
 
 class ProjectCard extends Component {
+    constructor() {
+        super()
+        this.state = {
+            status: 'successful',
+            lastRun: '24 minutes ago'
+        }
+    }
+
     componentDidMount() {
         // setInterval(() => {
         //     fetch('gitlab').then(response => this.setState({response}))
         // })
     }
 
-    render () {
-        return <div className="ProjectCard">
+    render() {
+        const { displayName, location } = this.props.project
+        const classList = ['ProjectCard']
+        classList.push(this.state.status)
+
+        return <div className={classList.join(' ')}>
+            <h2>{displayName}</h2>
+            <h4>{location}</h4>
         </div>
+
     }
 }
 
