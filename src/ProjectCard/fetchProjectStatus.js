aa772acd95efb652c36c05c63c6535b885bdd497
Austin McKinley <austinmckinley0@gmail.com>
2018-02-13 12:50:04 -0700

    Use base gitlab api url

--- src/ProjectCard/fetchProjectStatus.js
+++ src/ProjectCard/fetchProjectStatus.js
@@ -2,11 +2,12 @@ import { get } from '../util/fetchUtil'
 
 export default async (projectLocation, apiBase, apiToken) => {
     try {
-        const pipelinesUrl = `${apiBase}${encodeURIComponent(projectLocation)}/pipelines?private_token=${apiToken}`
+        const pipelinesUrl = `${apiBase}/projects/${encodeURIComponent(projectLocation)}/pipelines?private_token=${apiToken}`
         const pipelines = await get(pipelinesUrl)
         const filteredPipelines = pipelines.body.filter(p => p.status !== 'skipped')
         const pipelineId = filteredPipelines[0]['id']
-        const lastPipelineUrl = `${apiBase}${encodeURIComponent(projectLocation)}/pipelines/${pipelineId}?private_token=${apiToken}`
+
+        const lastPipelineUrl = `${apiBase}/projects/${encodeURIComponent(projectLocation)}/pipelines/${pipelineId}?private_token=${apiToken}`
         const lastPipelineResponse = await get(lastPipelineUrl)
         const lastPipeline = lastPipelineResponse.body
 
