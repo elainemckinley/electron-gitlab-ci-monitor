cfa51d18cbbb7320142b7b0331db46ed46291479
Austin McKinley <austinmckinley0@gmail.com>
2018-02-13 20:38:45 -0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "react/jsx-tag-spacing": 1,
+        "no-console": 0
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,18 @@ module.exports = {
     entry: ['babel-polyfill', './src/index.js'],
     output: {
         filename: 'index.bundle.js',
+        // eslint-disable-next-line no-undef
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         loaders: [{
             test: /\.jsx?/,
+            // eslint-disable-next-line no-undef
             include: path.resolve(__dirname, 'src'),
             loader: 'babel-loader'
         }, {
             test: /\.css/,
+            // eslint-disable-next-line no-undef
             include: path.resolve(__dirname, 'src'),
             loader: 'style-loader!css-loader'
         }]
